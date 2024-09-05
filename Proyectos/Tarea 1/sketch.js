618eee7410056f5fb37c49574f0036de974aa762
Cony-pin <constanza.pinto.a@mail.pucv.cl>
2024-09-05 14:22:08 -0400

    a

---
+++ Proyectos/Tarea 1/sketch.js
@@ -0,0 +1,121 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  noStroke()
+  
+//Primera línea
+  
+  fill("#9496c6");
+  ellipse(350,350,100,100);
+ 
+  fill("#635d8d");
+  ellipse(250,350,100,100);
+  
+  fill("#7468ca");
+  ellipse(150,350,100,100);
+ 
+  fill("#b094c6");
+  ellipse(50,350,100,100);
+  
+//Segunda línea
+  
+  fill("#635d8d");
+  ellipse(350,250,100,100);
+  
+  fill("#7468ca");
+  ellipse(250,250,100,100);
+  
+  fill("#b094c6");
+  ellipse(150,250,100,100);
+
+  fill("#9496c6");
+  ellipse(50,250,100,100);
+  
+//Tercera fila
+  
+  fill("#7468ca");
+  ellipse(350,150,100,100);
+
+  fill("#b094c6");
+  ellipse(250,150,100,100);
+
+  fill("#9496c6");
+  ellipse(150,150,100,100);
+  
+  fill("#635d8d");
+  ellipse(50,150,100,100);
+
+//Cuarta fila
+  
+  fill("#b094c6");
+  ellipse(350,50,100,100);
+
+  fill("#9496c6");
+  ellipse(250,50,100,100);
+  
+  fill("#635d8d");
+  ellipse(150,50,100,100);
+
+  fill("#7468ca");
+  ellipse(50,50,100,100);
+
+//Primera fila 2
+  
+  fill("#7468ca");
+  ellipse(350,350,50,50)
+  
+  fill("#b094c6");
+  ellipse(250,350,50,50);
+  
+  fill("#9496c6");
+  ellipse(150,350,50,50);
+  
+  fill("#635d8d");
+  ellipse(50,350,50,50);
+  
+//Segunda fila 2
+  
+  fill("#b094c6");
+  ellipse(350,250,50,50)
+  
+  fill("#9496c6");
+  ellipse(250,250,50,50);
+  
+  fill("#635d8d");
+  ellipse(150,250,50,50);
+  
+  fill("#7468ca");
+  ellipse(50,250,50,50);
+  
+// Tercera fila 2 
+   
+  fill("#9496c6");
+  ellipse(350,150,50,50);
+  
+  fill("#635d8d");
+  ellipse(250,150,50,50);
+
+  fill("#7468ca");
+  ellipse(150,150,50,50);
+  
+  fill("#b094c6");
+  ellipse(50,150,50,50);
+  
+//Cuarta fila 2
+  
+  fill("#635d8d");
+  ellipse(350,50,50,50);
+  
+  fill("#7468ca");
+  ellipse(250,50,50,50);
+  
+  fill("#b094c6");
+  ellipse(150,50,50,50);
+  
+  fill("#9496c6");
+  ellipse(50,50,50,50);
+}
